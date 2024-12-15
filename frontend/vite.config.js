a49d2c7700f7ce540caee30da47737f492866442
schmidie <philipp.schmid1@hs-augsburg.de>
2024-12-15 12:16:37 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_BASE_URL,
+        target: process.env.VITE_BASE_URL,
         changeOrigin: true,
         secure: false,
         headers: {
