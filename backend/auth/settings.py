3dcf1e2bc60181df3f6e513a1cb59893a0b7b950
schmidie <philipp.schmid1@hs-augsburg.de>
2024-12-15 12:53:07 +0100

    Update settings.py

--- backend/auth/settings.py
+++ backend/auth/settings.py
@@ -141,6 +141,7 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 ALLOWED_HOSTS = ['*']
 #CORS_ALLOWED_ORIGINS = [
 #    "http://localhost:5173",
@@ -154,4 +155,4 @@ STATIC_URL = "static/"
 
 
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+#DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
