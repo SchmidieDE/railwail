728d6340add1e4fd7ea7a90175797447ee75acd7
schmidie <philipp.schmid1@hs-augsburg.de>
2025-03-04 12:46:28 +0100

    jj

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -25,11 +25,11 @@ const config = {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			primary: {
-  				DEFAULT: '#BD34FE',
+  				DEFAULT: '#B247FE',
   				foreground: 'hsl(var(--primary-foreground))'
   			},
   			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
+  				DEFAULT: '#60B8FE',
   				foreground: 'hsl(var(--secondary-foreground))'
   			},
   			destructive: {
